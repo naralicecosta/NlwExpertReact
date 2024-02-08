fdcc588747ba00050e955718dd7a4d24f4ee42bb
naralicecosta <naralicecosta04@gmail.com>
2024-02-08 18:31:54 -0300

    style: :lipstick: criação do input

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -2,9 +2,17 @@ import logo from './assets/logo-nlw-expert.svg'
 
 export function App(){
   return (
-    <div className="mx-">
-      <img src={logo}/>
-      <input type="text" placeholder='Busque em suas notas...' />
+    <div className="mx-auto max-w6xl my-12">
+      <img src={logo} alt='NLW Expert'/>
+
+      <form className='w-full outline'>
+        <input
+         type="text" 
+         placeholder='Busque em suas notas...'
+         className='w-full bg-transparent text-3xl font-semibold outline-none tracking-tight placeholder:text-slate-500' />
+      </form>
+
+      
 
     </div>
   )
