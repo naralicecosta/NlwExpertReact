3638af17b5112908f0701623c7c10f8df8a2b1c6
naralicecosta <naralicecosta04@gmail.com>
2024-02-08 18:27:10 -0300

    feat: :sparkles: Começando app

--- nlw-expert-notes/src/app.tsx
+++ nlw-expert-notes/src/app.tsx
@@ -1,3 +1,11 @@
+import logo from './assets/logo-nlw-expert.svg'
+
 export function App(){
-  return <h1>hello world</h1>
+  return (
+    <div className="mx-">
+      <img src={logo}/>
+      <input type="text" placeholder='Busque em suas notas...' />
+
+    </div>
+  )
 }
