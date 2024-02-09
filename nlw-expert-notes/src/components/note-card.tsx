048751279b5ac52ff0135180511bd310028880e8
naralicecosta <naralicecosta04@gmail.com>
2024-02-09 18:15:18 -0300

    feat: :sparkles: ajustando botão

--- nlw-expert-notes/src/components/note-card.tsx
+++ nlw-expert-notes/src/components/note-card.tsx
@@ -1,11 +1,11 @@
 export function NoteCard(){
     return (
 
-        <div className='rounded-md bg-slate-800 p-5 space-y-3 overflow-hidden relative'>
+        <button className='rounded-md text-left bg-slate-800 p-5 space-y-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-line-400'>
         <span className='text-sm font-medium text-slate-300'> há dois dias</span>
           <p className='text-sm leading-6 text-slate-400'>Grave ...</p>
           <div className='absolute bottom-0 right-0 left-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none' />
 
-        </div>
+        </button>
     )
 }
